be5e5ac4944ddce2b101780c7ec60f1b28e3e01e
shah014 <anilshah65156@gmail.com>
2021-03-17 01:23:55 +0545

    worked_json_read_write

---
+++ 18.py
@@ -0,0 +1,20 @@
+# importing package for dealing with json in python
+import json
+
+print(dir(json))
+
+# to learn more on json we use
+# help(json)
+
+# dictionary mapping 'name' to your name and 'age' to your age
+
+my_info = {"name": "Anil Shah", "age": 25}
+
+# writing it to a json string
+with open("my_info.json", "w") as write:
+    json.dump(my_info, write)
+
+# back to python form from json
+with open("my_info.json", "r") as read:
+    res = json.load(read)
+print(res)
