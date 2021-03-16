6b59aef00885bcb9be88c5cf3b626177b9ebd676
shah014 <anilshah65156@gmail.com>
2021-03-16 22:35:16 +0545

    binary_searched

---
+++ 9.py
@@ -0,0 +1,20 @@
+lst = []
+n = int(input("Enter the elements in list: "))
+for i in range(n):
+    k = int(input("Enter element to add: "))
+    lst.append(k)
+print(lst)
+f = int(input("Enter the element to find: "))
+
+
+def binary_search(lst, f):
+    for i in range(len(lst)):
+        if f in lst:
+            return lst.index(f)
+        else:
+            return -1
+
+
+print(binary_search(lst, f))
+
+
